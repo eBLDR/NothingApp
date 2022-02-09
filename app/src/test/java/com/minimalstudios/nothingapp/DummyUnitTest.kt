523dc6adcf91c0571e58308f9afd2f5a8d29b33b
MinimalStudios <developer.minimalstudios@gmail.com>
2022-02-09 16:03:26 +0100

    Tests

---
+++ app/src/test/java/com/minimalstudios/nothingapp/DummyUnitTest.kt
@@ -0,0 +1,12 @@
+package com.minimalstudios.nothingapp
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class DummyUnitTest {
+
+    @Test
+    fun alwaysTrue() {
+        assertTrue(true)
+    }
+}
