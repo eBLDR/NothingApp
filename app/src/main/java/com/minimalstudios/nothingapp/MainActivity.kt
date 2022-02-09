5ff8c9e87fb0608d82614abdcb843255b396a204
MinimalStudios <developer.minimalstudios@gmail.com>
2022-02-09 08:07:58 +0100

    Clean

--- app/src/main/java/com/minimalstudios/nothingapp/MainActivity.kt
+++ app/src/main/java/com/minimalstudios/nothingapp/MainActivity.kt
@@ -5,11 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.minimalstudios.nothingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
